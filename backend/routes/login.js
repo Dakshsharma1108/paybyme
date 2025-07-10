b273f20a70b87c8faad0eb83a504c3071f7c1774
Daksh <dakshsharmab1@gmail.com>
2025-07-10 11:50:38 +0530

    adding jwt in login page

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,6 +4,7 @@ import User from '../db.js';
 // using  username create jwt
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import { jwtSecret } from '../jwt.js'; // Import the JWT secret from jwt.js
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
@@ -19,7 +20,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ error: 'Invalid email or password' });
         }
         // Create JWT token with email
-        const token = jwt.sign({ email: user.email }, '12333', { expiresIn: '1h' });
+        const token = jwt.sign({ email: user.email },jwtSecret, { expiresIn: '1h' });
         console.log('JWT Token:', token);
         res.status(200).json({ message: 'User logged in successfully', user, token });
     } catch (error) {
