eff6577e48c855990447428fe3ba04f272fc95ce
Daksh <dakshsharmab1@gmail.com>
2025-07-12 11:59:44 +0530

    remove commented code

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,3 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
-// mongoose.connect(process.env.MONGODB_URL);
-
-// const user = mongoose.Schema({
-//     id: String,
-//     email: {
-//         type: String,
-//         required: true,
-//         match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
-//     },
-//     password: String
-
-// })
-
-// const User = mongoose.model('User', user);
-
-// export default User;
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,32 +1,3 @@
-// import express from 'express';
-// import User from '../db.js';
-// import bcrypt from 'bcryptjs';
-
-// const router = express.Router();
-// const globalid = 0;
-// router.post('/signup', async (req, res) => {
-//     try {
-//         const { email, password} = req.body;
-//         if (!email || !password) {
-//             return res.status(400).json({ error: 'All fields are required' });
-//         }
-//         // Check if user already exists
-//         const existingUser = await User.findOne({ email });
-//         if (existingUser) {
-//             return res.status(400).json({ error: 'User already exists' });
-//         }
-//         // Hash the password
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         const newUser = new User({ email, password: hashedPassword , id: "user" + globalid++ });
-//         await newUser.save();
-//         res.status(201).json({ message: 'User created successfully' });
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// });
-
-// export default router;
-
 import express from 'express';
 import User from '../db.js';
 import bcrypt from 'bcryptjs';
